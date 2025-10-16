5ca614c3b571e11162631f896ac678c0380786a6
sartoricobra <rafaelsartori@gmail.com>
2025-10-15 23:40:09 -0400

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,6 +1,8 @@
 <?php
 $data = json_encode([
     "nome" => $_POST["nome"] ?? "desconhecido",
+    "email" => $_POST["email"] ?? "desconhecido",
+    "telefone" => $_POST["telefone"] ?? "desconhecido",
     "mac" => $_POST["mac"] ?? "desconhecido",
     "ip" => $_POST["ip"] ?? "desconhecido",
     "horario" => $_POST["horario"] ?? date("c")
@@ -9,6 +11,6 @@ $data = json_encode([
 file_put_contents("sessoes.log", $data . PHP_EOL, FILE_APPEND);
 
 // Redireciona para o login trial no MikroTik
-header("Location: http://192.168.88.1/login?username=trial");
+header("Location: http://192.168.77.1/login?username=trial");
 exit;
 ?>
